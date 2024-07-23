2e9a35734c7c382ecc2bab2cc0d5298839c871eb
Trixarian <admin@trixarian.net>
2024-07-23 11:12:37 +0200

    Stripped out The Endhanced Network API

--- main.cs
+++ main.cs
@@ -40,12 +40,12 @@ public class Main : BasePlugin
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
     public const string PluginVersion = "2024.0721.200.11100"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.0.0 Canary 11 Hotfix 1";
+    public const string PluginDisplayVersion = "2.0.0";
     public const string SupportedVersionAU = "2024.6.18";
 
     /******************* Change one of the three variables to true before making a release. *******************/
-    public static readonly bool Canary = true; // ACTIVE - Latest: V2.0.0 11 Hotfix 1
-    public static readonly bool fullRelease = false; // INACTIVE - Latest: V1.6.0
+    public static readonly bool Canary = false; // ACTIVE - Latest: V2.0.0 11 Hotfix 1
+    public static readonly bool fullRelease = true; // INACTIVE - Latest: V2.0.0
     public static readonly bool devRelease = false; // INACTIVE - Latest: V2.0.0 Dev 25
 
     public static bool hasAccess = true;
