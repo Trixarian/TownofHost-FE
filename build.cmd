48b320bcdb1cfc474357c4cca31f30902f57db6b
Trixarian <admin@trixarian.net>
2024-07-24 08:57:04 +0200

    Build script now opens path to dll after building

--- build.cmd
+++ build.cmd
@@ -1,6 +1,10 @@
 @echo off
-echo Building TOHE from commandline...
+set output_path=%p2f%bin\Release\net6.0
+
+echo Building TOHE...
 @echo on
 dotnet build -c Release
 @echo off
 pause
+cd "%output_path%"
+explorer .
